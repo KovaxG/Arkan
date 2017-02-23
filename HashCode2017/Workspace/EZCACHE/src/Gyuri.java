59cb707a5e6acc1504c38ea0c5ec0444dfce95a6
KovaxG <georgesmth202@gmail.com>
2017-02-23 22:12:44 +0200

    bla

--- HashCode2017/Workspace/EZCACHE/src/Gyuri.java
+++ HashCode2017/Workspace/EZCACHE/src/Gyuri.java
@@ -1,9 +1,40 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Gyuri {
 	public static void main(String args[]) {
 		
+		Robert.Parse();
+		
+		// Create an arrayList with all the requests
+		List<Request> allRequests = new ArrayList<Request>();
+		for (EndPoint ep : Robert.endpoints) {
+			for (Request req : ep.getRequestList()) {
+				allRequests.add(req);
+			}
+		}
+		
+		// Sort the list using the demand field
+		Collections.sort(allRequests, (o1, o2) -> o1.compareTo(o2));
+		
+		// Go through all requests
+		for (Request r : allRequests) {
+			System.out.print(r.toString());
+			EndPoint endP = findEndPoint(r);
+			
+			int minLag = 9999999;
+			for (Pair<Cache, Integer> pair : endP.getChacheList()) {
+				Cache c = pair.getFirst();
+				int lag = pair.getSecond();
+				if (c.getSize())
+			}
+		}
 	}
+} // End of Class Gyuri
+
+public static EndPoint findEndPoint(Request r) {
+	return null;
 }
 
 class Pair <T1,T2> {
@@ -27,11 +58,13 @@ class Pair <T1,T2> {
 	public void setSecond(T2 second) {
 		this.second = second;
 	}
-}
+} // End of Pair
 
 class Video {
 	private int id;
 	private int size;
+	
+	public Video() {}
 	
 	public Video(int id, int size) {
 		this.id = id;
@@ -54,12 +87,18 @@ class Video {
 		this.size = size;
 	}
 	
-	
+	public boolean equals(Object o) {
+		if (o == null) return false;
+		if (!(o instanceof Video)) return false;
+		
+		Video v = (Video) o;
+		return this.id == v.getId(); 
+	}
 } // End of Video
 
 class Cache {
 	private int id;
-	private static int size;
+	private int size;
 	
 	ArrayList<Video> videos = new ArrayList<Video>();
 
@@ -71,12 +110,12 @@ class Cache {
 		this.id = id;
 	}
 
-	public static int getSize() {
+	public int getSize() {
 		return size;
 	}
 
-	public static void setSize(int size) {
-		Cache.size = size;
+	public void setSize(int size) {
+		this.size = size;
 	}
 
 	public ArrayList<Video> getVideos() {
@@ -86,8 +125,6 @@ class Cache {
 	public void setVideos(ArrayList<Video> videos) {
 		this.videos = videos;
 	}
-	
-	
 } // End of Cache
 
 class Request {
@@ -107,14 +144,31 @@ class Request {
 		this.demand = demand;
 	}
 	
+	public boolean equals(Object o) {
+		if (o == null) return false;
+		if (!(o instanceof Request)) return false;
+		
+		Request r = (Request) o;
+		return r.getVideo().equals(this.video) 
+			   && r.getDemand() == this.demand;
+	}
 	
+	public int compareTo(Request r) {
+		return this.demand - r.getDemand();
+	}
+	
+	public String toString() {
+		return String.format("%d, %d", video.getId(), demand);
+	}
 } // End of Request
 
 class EndPoint {
+	
 	private int id;
 	private ArrayList<Request> requestList = new ArrayList<Request>();
 	private ArrayList<Pair<Cache, Integer>> chacheList = new ArrayList<Pair<Cache, Integer>>();
 	private int dataCenterLatency;
+	
 	public int getId() {
 		return id;
 	}
@@ -139,11 +193,10 @@ class EndPoint {
 	public void setDataCenterLatency(int dataCenterLatency) {
 		this.dataCenterLatency = dataCenterLatency;
 	}
-	
-	
-}
+} // End of EndPoint
 
 class DataCenter {
+	
 	private ArrayList<Video> videoList = new ArrayList<Video>();
 
 	public ArrayList<Video> getVideoList() {
@@ -153,6 +206,4 @@ class DataCenter {
 	public void setVideoList(ArrayList<Video> videoList) {
 		this.videoList = videoList;
 	}
-	
-	
-}
+} // End of DataCenter

4c4c98a6de4f360feb46964a54b56967e4636915
robertkajnak <hezirisz@gmail.com>
2017-02-23 21:47:30 +0200

    partialparse

--- Hashcode2017/Workspace/EZCACHE/src/Parser.java
+++ Hashcode2017/Workspace/EZCACHE/src/Parser.java
@@ -19,20 +19,49 @@ public class Parser {
 		try {
 			br = new BufferedReader(new FileReader("me_at_the_zoo.in"));
 			
-
-			videoCount = br.read();
-			endpointCount  = br.read();
-			requestDesctriptionCount  = br.read();
-			cacheCount = br.read();
-			cacheSize = br.read();
+			String line = br.readLine();
+			Scanner sc = new Scanner(new FileReader("me_at_the_zoo.in"));
 			
-			System.out.println(videoCount + "; " + endpointCount + "; " + requestDesctriptionCount + "; " + cacheCount + "; " + cacheSize);
+			videoCount = sc.nextInt();
+			endpointCount  = sc.nextInt();
+			requestDesctriptionCount  = sc.nextInt();
+			cacheCount = sc.nextInt();
+			for (int i = 0; i < cacheCount; i++) {
+				Cache c =new Cache();
+				c.setId(i);
+				caches.add(c);
+			}
+			cacheSize = sc.nextInt();
+			Cache.setSize(cacheSize);
 			
+			//System.out.println(videoCount + "; " + endpointCount + "; " + requestDesctriptionCount + "; " + cacheCount + "; " + cacheSize);
+			///section 1 - vidoesizez
 			for (int i = 0; i < videoCount; i++) {
-				videos.add(new Video(i, br.read()));
-				System.out.println(videos.get(videos.size()-1).getSize());
+				videos.add(new Video(i, sc.nextInt()));
+				//System.out.println(videos.get(videos.size()-1).getSize());
 			}
 			
+			///sectiion 2 - endpoint latencies
+			for (int i = 0; i < endpointCount; i++) {
+				EndPoint ep = new EndPoint();
+				ep.setId(i);
+				ep.setDataCenterLatency(sc.nextInt());
+				System.out.println("Endpoint: " + i);
+				System.out.println("DatacenterLatancy: " + ep.getDataCenterLatency());
+				///connected to n caches
+				
+				int m = sc.nextInt();
+				System.out.println("Connected to " + m + " caches");
+				for (int j=0; j < m; j++){
+					int toCacheNr = sc.nextInt();
+					int toCacheLatency = sc.nextInt();
+					
+					System.out.println("to Cache " + toCacheNr + " with Latency: " + toCacheLatency);
+					ep.getChacheList().add(new Pair<Cache,Integer>(caches.get(toCacheNr),toCacheLatency));
+				}
+			}
+			
+			sc.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
